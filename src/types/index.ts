540478039d905cd035538680da0b025be4f37995
MarielBaezSanchez <marielbaezsanchez991@gmail.com>
2025-02-11 13:18:29 -0600

    11 show recipes in DrinkCard components

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,8 @@
 import { z } from "zod";
-import { CategoriesApiResponseSchema, RecipesAPIResponseSchema, SearchFiltersSchema } from "../utils/recipes-schema";
+import { CategoriesApiResponseSchema, DrinkSchema, RecipesAPIResponseSchema, SearchFiltersSchema } from "../utils/recipes-schema";
 
 
 export type Categories = z.infer<typeof CategoriesApiResponseSchema>
 export type SearchFilter = z.infer<typeof SearchFiltersSchema>
 export type Recipes = z.infer<typeof RecipesAPIResponseSchema>
+export type Drink = z.infer<typeof DrinkSchema>
